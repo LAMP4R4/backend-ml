10a18dc7bc60f8507b1dae14813cbc2901aaf894
LAMP4R4 <kayjack462@gmail.com>
2026-07-28 10:33:43 -0600

    Actualizacion

--- src/main/java/com/lamp/amazonas/controller/ProductoController.java
+++ src/main/java/com/lamp/amazonas/controller/ProductoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/productos") // mapeo general productos
-// @CrossOrigin(origins = "http://localhost:5173") // permiso de react para
-// consumir la api
 @RequiredArgsConstructor
 public class ProductoController {
 
--- src/main/java/com/lamp/amazonas/controller/VentasController.java
+++ src/main/java/com/lamp/amazonas/controller/VentasController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +21,6 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/ventas")
-@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class VentasController {
 
